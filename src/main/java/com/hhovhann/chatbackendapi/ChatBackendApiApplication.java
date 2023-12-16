20a579c0c8e11dd3d4553c7be83179931a186e3a
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2023-12-16 18:22:24 +0400

    Add application skeleton

---
+++ src/main/java/com/hhovhann/chatbackendapi/ChatBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.hhovhann.chatbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBackendApiApplication.class, args);
+	}
+
+}
